fcdc1a6040be48121d5ff4bdaf3532e6f8e52129
Lochlan Walshe <33631368+Lochaus@users.noreply.github.com>
2024-03-25 17:15:45 +0000

    Day 5 Part 2
    Day 5 completed with the solution for part 2 working

--- Day5.py
+++ Day5.py
@@ -21,19 +21,41 @@ def check_banned_strings(word):
             return False
     return True
 
+def check_pair_double_letter(word):
+    initial_word = word
+    for i in range(len(word)-1):
+        letter_pair = word[i:i+2]
+        word = word.split(letter_pair)
+        if len(word) > 2:
+            return True
+        else:
+            word = initial_word
+    return False
+
+def check_gapped_double_letter(word):
+    for i in range(len(word)-2):
+        if word[i] == word[i+2]:
+            return True
+    return False
+
 
 def Part1(input_lines):
     nice_string_counter = 0
     for line in input_lines:
         line = line.replace("\n", "")
         if check_triple_vowel(line) and check_double_letter(line) and check_banned_strings(line):
-            print(nice_string_counter, line)
             nice_string_counter += 1
     return nice_string_counter
 
 
 def Part2(input_lines):
-    return 0
+    nice_string_counter = 0
+    for line in input_lines:
+        line = line.replace("\n", "")
+        if check_pair_double_letter(line) and check_gapped_double_letter(line):
+            print(line)
+            nice_string_counter += 1
+    return nice_string_counter
 
 
 # Open input data as input file
